b303f94315d3290938e9f4b4738b200ab4056f47
prokoninaritai <s0a7c5h1i@gmail.com>
2024-12-03 22:14:50 +0900

    変更と自分なりの解釈

--- drill32.rb
+++ drill32.rb
@@ -3,7 +3,7 @@ class Student
     @name = name
   end
 
-  def self.introduce
+  def introduce
     puts "私の名前は#{@name}です。"
   end
 end
@@ -11,3 +11,8 @@ end
 student = Student.new
 student.set_name("山田太郎")
 student.introduce
+
+#selfをつけることでintroduceがクラスメソッドになってしまっていた
+#クラスメソッドの中でインスタンス変数は使えない
+#クラスメソッドはインスタンスstudentから呼び出すことはできない
+#selfを消し、introduceをインスタンスメソッドとすることで、エラーが解消された。
