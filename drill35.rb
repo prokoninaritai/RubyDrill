9c76961949337aaedcd8f8ae665ca30c73d2758a
prokoninaritai <s0a7c5h1i@gmail.com>
2024-12-07 16:36:42 +0900

    Update drill35.rb

--- drill35.rb
+++ drill35.rb
@@ -1,5 +1,6 @@
 def near_ten(integer)
-  if integer % 10 <= 2 || integer % 10 >= (10 - 2)
+  quotie = integer % 10
+  if quotie <= 2 || quotie >= 8
     puts "True"
   else
     puts "False"
@@ -9,3 +10,5 @@ end
 near_ten(12)
 near_ten(17)
 near_ten(19)
+
+#integer % 10はquotient(商)に格納する
