db175e699195f6801e5542e99eafb150d83328f5
prokoninaritai <s0a7c5h1i@gmail.com>
2025-01-11 10:57:48 +0900

    回答

--- drill66.rb
+++ drill66.rb
@@ -1,25 +1,43 @@
-def registration_student
+def registration_student(students)
   # 生徒の名前と年齢を登録できるようにしなさい
   student = {}
   puts '生徒名を入力してください'
+  student[:name] = gets.chomp
   puts '生徒の年齢を入力してください'
+  student[:age] = gets.to_i
 
   # 登録した生徒の国語、数学、英語の点数を登録できるようにしなさい
   puts "国語の得点は？"
+  student[:japanese] = gets.to_i
   puts "数学の得点は？"
+  student[:math] = gets.to_i
   puts "英語の得点は？"
+  student[:english] = gets.to_i
+  students << student
 end
 
-def show_student_name
+def show_student_name(students)
   # 登録された生徒の名前を番号を振って表示しなさい
-  puts '見たい生徒の番号を入力してください'
+  i = 0
+  students.each_with_index do |student, i|
+    puts "[#{i+1}]:#{student[:name]}"
+    i += 1
+  end
 
-  # 選択された生徒の名前、年齢、国語、数学、英語の点数を表示できるようにしなさい
-  puts "名前:"
-  puts "年齢:"
-  puts "国語:"
-  puts "数学:"
-  puts "英語:"
+  puts '見たい生徒の番号を入力してください'
+  num = gets.to_i - 1
+
+  if student = students[num] 
+    # 選択された生徒の名前、年齢、国語、数学、英語の点数を表示できるようにしなさい
+    puts "名前:#{student[:name]}"
+    puts "年齢:#{student[:age]}"
+    puts "国語:#{student[:japanese]}"
+    puts "数学:#{student[:math]}"
+    puts "英語:#{student[:english]}"
+  else
+    puts "無効な値です"
+    show_student_name(students)
+  end
 end
 
 students = []
@@ -31,11 +49,12 @@ while true
   puts '[3]終了する'
   input = gets.to_i
   if input == 1
-    registration_student
+    registration_student(students)
   elsif input == 2
-    show_student_name
+    show_student_name(students)
   elsif input == 3
     # アプリケーションを終了させなさい
+    exit
   else
     puts '無効な値です'
   end
